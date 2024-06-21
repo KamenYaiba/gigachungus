1661901411f3e9e9f766d2a35ff141deffd87c7c
ahmadammarjr <110498902+ahmadammarjr@users.noreply.github.com>
2024-06-21 16:32:26 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,6 +1,6 @@
 import telebot
 from config import(report_a_command, report_b_command, invalid_format_warning,wrong_info,
-                   menu, greet, report_a_manual)
+                   menu, greet, report_a_manual, language_changed)
 from keys import TOKEN, REQUEST_KEYS
 from functions import(report_a_request, report_b_request, language, error_log, log, report_log,
                        add_to_arabic_users, remove_from_arabic_users)
@@ -15,3 +15,15 @@ def start_handler(msg):
     lang = language(id)
     bot.send_photo(id, photo=open(greet[lang], 'rb'), caption=menu[lang])
     log(msg)
+
+
+def change_language_handler(msg):
+    lang = 0 if msg.text.strip('/') == 'ar' else 1
+    id = msg.chat.id
+    if lang == 0:
+        add_to_arabic_users(id)
+    else:
+        remove_from_arabic_users(id)
+    bot.reply_to(msg, language_changed[lang])
+
+
