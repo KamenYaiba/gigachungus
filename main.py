8acd3c0ba42da693e6573d99ddac1f02cd2803f3
ahmadammarjr <110498902+ahmadammarjr@users.noreply.github.com>
2024-06-22 07:04:44 +0300

    fixed regexes

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def change_lang_to_ar(msg):
     change_language_handler(msg)
 
 
-@bot.message_handler(regexp='^' + report_a_command+'|'+report_b_command)
+@bot.message_handler(regexp='^' + '['+report_a_command+report_b_command+']')
 def report_a(msg):
     report_ab_handler(msg)
 
