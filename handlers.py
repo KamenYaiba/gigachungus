d0fb6fb0b2cc3d2825e62d7e44762f0cb382fc4e
mio <110498902+KamenYaiba@users.noreply.github.com>
2025-02-16 22:54:22 +0300

    Fix a Bug

--- handlers.py
+++ handlers.py
@@ -35,7 +35,7 @@ def report_ab_handler(msg):
     log(msg)
     chat_id = msg.chat.id
     lang = language(chat_id)
-    text = msg.text
+    text = msg.text.lower()
     report = report_a_request(text, lang) if text.startswith('a') else report_b_request(text, lang)
     if report == -1:
         bot.reply_to(msg, text=invalid_format_warning[lang])
